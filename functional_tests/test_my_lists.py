a70ca859c3fd3f1a17c8d128bd5304708f5780d3
Harry <hjwp2@cantab.net>
2016-08-18 05:03:58 +0100

    fixup test_my_lists for assert_logged_in

--- functional_tests/test_my_lists.py
+++ functional_tests/test_my_lists.py
@@ -1,9 +1,9 @@
 from django.conf import settings
 from django.contrib.auth import BACKEND_SESSION_KEY, SESSION_KEY, get_user_model
-User = get_user_model()
 from django.contrib.sessions.backends.db import SessionStore
 
 from .base import FunctionalTest
+User = get_user_model()
 
 
 class MyListsTest(FunctionalTest):
@@ -27,9 +27,9 @@ class MyListsTest(FunctionalTest):
     def test_logged_in_users_lists_are_saved_as_my_lists(self):
         email = 'edith@example.com'
         self.browser.get(self.server_url)
-        self.wait_to_be_logged_out(email)
+        self.assert_logged_out(email)
         # Edith is a logged-in user
         self.create_pre_authenticated_session(email)
         self.browser.get(self.server_url)
-        self.wait_to_be_logged_in(email)
+        self.assert_logged_in(email)
 
